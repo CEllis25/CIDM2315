c809cbdfbc73d63b6a6bd1d95d7bea15504919fa
CEllis25 <cdellis3@buffs.wtamu.edu>
2025-01-27 21:24:49 -0600

    Add files via upload

--- Homework1.cs
+++ Homework1.cs
@@ -3,6 +3,11 @@ class Program
 {
     static void Main(string[ ] args)
     {
-        // Code Here 
+        {
+            double x = 2.5;
+            double y = 3.3;
+            double z = 4 * x * x + 3 * y;
+            Console.WriteLine(z);
+        }
  			     }
 }
